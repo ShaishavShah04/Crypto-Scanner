578bd6aaf3677c7e7d4842c678ab528b6b3af965
Shaishav Shah <shaishav.shah04@gmail.com>
2021-08-14 10:42:25 -0600

    Added the increasing price check

--- testing.py
+++ testing.py
@@ -22,12 +22,23 @@ def get_tickers( base = "BUSD" ):
     return symbols
 
 
-df = pd.DataFrame( exchange.fetch_ohlcv("DOGE/BUSD","15m", limit=3),
+df = pd.DataFrame( exchange.fetch_ohlcv("ADA/BUSD","1d", limit=3),
                    columns = ["time", "open", "high", "low", "close", "volume"] )
-
-pp(df)
-
-print(df["volume"].iloc[-1])
 
 def explosive_volume(df_volume):
     return df_volume.iloc[-1] > ( df_volume.iloc[-1] * 3 )
+
+def increasing_price(df, symbol):
+    ticker_info = exchange.fetch_tickers(symbol)[symbol]
+    # Last High / Open
+    openPrice = df.iloc[-2]["open"]
+    closePrice = df.iloc[-2]["close"]
+    previous_timeframe_peak = closePrice if closePrice > openPrice else openPrice
+    # Comparing Current price to yesterdays price
+    price = ( ticker_info["ask"] + ticker_info["bid"] ) / 2
+    # print(f"Open Price: {openPrice}")
+    # print(f"Close Price: {closePrice}")
+    # print(price)
+    return price > previous_timeframe_peak
+
+
