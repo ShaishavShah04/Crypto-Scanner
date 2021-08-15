97da21353efd30ff1b395c9e67f769d8e8be224e
Shaishav Shah <shaishav.shah04@gmail.com>
2021-08-15 09:44:21 -0600

    Added the function to generate the potential cryptos meeting the criteria. Code works. I need to rethink my strategy.

--- testing.py
+++ testing.py
@@ -8,12 +8,9 @@ from pprint import pprint as pp
 import ta
 import pandas as pd
 
-
 exchange = ccxt.binance()
-data = exchange.load_markets()
 
-
-def get_tickers( base = "BUSD" ):
+def get_tickers(data, base = "BUSD" ):
     # Getting all symbols
     symbols = []
     for symbol in data:
@@ -21,24 +18,38 @@ def get_tickers( base = "BUSD" ):
             symbols.append(symbol)
     return symbols
 
-
-df = pd.DataFrame( exchange.fetch_ohlcv("ADA/BUSD","1d", limit=3),
-                   columns = ["time", "open", "high", "low", "close", "volume"] )
-
 def explosive_volume(df_volume):
-    return df_volume.iloc[-1] > ( df_volume.iloc[-1] * 3 )
+    return df_volume.iloc[-1] > ( df_volume.iloc[-2] * 2 )
 
-def increasing_price(df, symbol):
+def price_level(df, symbol):
     ticker_info = exchange.fetch_tickers(symbol)[symbol]
     # Last High / Open
     openPrice = df.iloc[-2]["open"]
     closePrice = df.iloc[-2]["close"]
-    previous_timeframe_peak = closePrice if closePrice > openPrice else openPrice
+    # 
+    if closePrice > openPrice:
+        previous_timeframe_peak = closePrice
+        previous_timeframe_low = openPrice
+    else:
+        previous_timeframe_peak = openPrice
+        previous_timeframe_low = closePrice
     # Comparing Current price to yesterdays price
     price = ( ticker_info["ask"] + ticker_info["bid"] ) / 2
-    # print(f"Open Price: {openPrice}")
-    # print(f"Close Price: {closePrice}")
-    # print(price)
+    # Result
+    if price > previous_timeframe_peak:
+        return "Up"
+    if price < previous_timeframe_low:
+        return "Down"
     return price > previous_timeframe_peak
 
-
+def generate_bullish():
+    data = exchange.load_markets()
+    symbols = get_tickers(data)
+    filtered = []
+    for symbol in symbols:
+        print(f"Testing {symbol}...")
+        df = pd.DataFrame( exchange.fetch_ohlcv(symbol,"1d", limit=3), columns = ["time", "open", "high", "low", "close", "volume"] )
+        if explosive_volume(df["volume"]) and price_level(df, symbol) == "Up":
+            filtered.append(symbol)
+    
+    print(filtered)
