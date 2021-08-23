5b8d4def4e71e042a4b1abcfcd10c8f6102124ff
Shaishav Shah <shaishav.shah04@gmail.com>
2021-08-23 11:46:05 -0600

    Fixed over-polling the API

--- scanner.py
+++ scanner.py
@@ -8,23 +8,32 @@ from pprint import pprint as pp
 import ta
 import pandas as pd
 
-exchange = ccxt.binance()
+exchange = ccxt.binance({
+    'enableRateLimit': True,
+})
 
-def get_tickers(data, base = "BUSD" ):
+def get_tickers(base = "BUSD"):
+    busd = []
+    market = exchange.fetch_markets()
     # Getting all symbols
-    symbols = []
-    for symbol in data:
-        if symbol.split("/")[-1] == base:
-            symbols.append(symbol)
-    return symbols
+    for info in market:
+        if info.get("quote") == base:
+            busd.append(info.get("symbol"))
+    return busd
+
 
 def explosive_volume(df_volume):
     return df_volume.iloc[-1] > ( df_volume.iloc[-2] * 2 )
 
+
 def bullish_engulfing(df, ticker_info):
     # Last High / Open
-    openPrice = df.iloc[-2]["open"]
-    closePrice = df.iloc[-2]["close"]
+    try:
+        openPrice = df.iloc[-2]["open"]
+        closePrice = df.iloc[-2]["close"]
+    except IndexError:
+        print("-- Not enough data. Probably a new listing!")
+        return False
     # Checking if last day was down
     if (closePrice < openPrice):
         price = ( ticker_info["ask"] + ticker_info["bid"] ) / 2
@@ -35,8 +44,12 @@ def bullish_engulfing(df, ticker_info):
 def price_level(df, ticker_info):
     
     # Last High / Open
-    openPrice = df.iloc[-2]["open"]
-    closePrice = df.iloc[-2]["close"]
+    try:
+        openPrice = df.iloc[-2]["open"]
+        closePrice = df.iloc[-2]["close"]
+    except IndexError:
+        print("-- Not enough data. Probably a new listing!")
+        return False
     # 
     if closePrice > openPrice:
         previous_timeframe_peak = closePrice
@@ -54,14 +67,17 @@ def price_level(df, ticker_info):
     return price > previous_timeframe_peak
 
 def generate_bullish():
-    data = exchange.load_markets()
-    symbols = get_tickers(data)
+    symbols = get_tickers()
     filtered = []
+    all_tickers = exchange.fetch_tickers()
 
     for symbol in symbols:
         print(f"Testing {symbol}...")
-        df = pd.DataFrame( exchange.fetch_ohlcv(symbol,"1d", limit=3), columns = ["time", "open", "high", "low", "close", "volume"] )
-        ticker_info = exchange.fetch_tickers(symbol)[symbol]
+        # Fetching Data
+        df = pd.DataFrame( exchange.fetch_ohlcv(symbol,"15m", limit=3), columns = ["time", "open", "high", "low", "close", "volume"] )
+        ticker_info = all_tickers.get(symbol)
+        # Some Basic things we need
+
         # Criteria
         upward_trend = (price_level(df, ticker_info) == "Up") and explosive_volume(df["volume"])
         bullish_engulfing_check = bullish_engulfing(df, ticker_info)
